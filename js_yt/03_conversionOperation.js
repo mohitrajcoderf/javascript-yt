8390533342fa7ac480751f9dc3a5e77b34424417
Mohit Raj <161021797+mohitrajcoderf@users.noreply.github.com>
2024-07-17 12:00:03 +0000

    conversion

--- js_yt/03_conversionOperation.js
+++ js_yt/03_conversionOperation.js
@@ -1,7 +1,27 @@
-let score = 33
+let score = null
 let number
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let ValueInNumber = number(score)
-console.log(typeof ValueInNumber)
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber)
+// console.log( ValueInNumber)
+
+
+/*
+// 33=> 33 number
+
+33 == 33asd string
+null = object
+
+*/
+
+let IsLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+
+ 
+console.log(booleanIsLoggedIn)
+ 
+// 1 == true ; 0 == false
+// "" == false : "mohit" == true

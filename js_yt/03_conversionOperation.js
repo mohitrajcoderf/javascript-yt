19adff4ed94ab9d9c1eab6cb77ee667818b32c8a
Mohit Raj <161021797+mohitrajcoderf@users.noreply.github.com>
2024-07-18 11:38:43 +0000

    hey

--- js_yt/03_conversionOperation.js
+++ js_yt/03_conversionOperation.js
@@ -30,5 +30,23 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "mohit"
+let str2 = " raj"
+
+let str3 = str1 + str2
+
+console.log(str3);
